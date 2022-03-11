ad186d946cd76a1ca62ae2226692b5bcf92c32c0
Matt Lampitt <mlampitt10@gmail.com>
2022-03-10 18:50:06 -0600

    updated shows

---
+++ tv_shows/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # localhost:8000/
+    path('', views.index),
+    # localhost:8000/tv_shows
+    path('shows/', views.index),
+    # localhost:8000/tv_shows/new
+    path('new', views.new),
+    #localhost:8000/tv_shows/create
+    path('create', views.create),
+    # localhost:8000/tv_shows/<show_id>/edit
+    path('<int:show_id>/edit', views.edit),
+    # localhost:8000/tv_shows/<show_id>/update
+    path('<int:show_id>/update', views.update),
+    # localhost:8000/tv_shows/<show_id>
+    path('<int:show_id>', views.show),
+    # localhost:8000/tv_shows/<show_id>/delete
+    path('<int:show_id>/delete', views.delete),
+]
